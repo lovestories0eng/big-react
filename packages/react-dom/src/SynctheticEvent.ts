6cab15a508d95a75a2a0b1c0bc6ff2afba493522
panshihuang <panshihuang@bytedance.com>
2023-07-11 18:57:46 +0800

    feat: 完成mount与update

---
+++ packages/react-dom/src/SynctheticEvent.ts
@@ -0,0 +1,11 @@
+import { Props } from 'shared/ReactTypes';
+
+export const elementPropsKey = '__props';
+
+export interface DOMElement extends Element {
+	[elementPropsKey]: Props;
+}
+
+export function updateFiberProps(node: DOMElement, props: Props) {
+	node[elementPropsKey] = props;
+}
